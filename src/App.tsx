618f0be03a16f9986e5ff3a15006064fa9fd8314
Andreas Plado <plado.andreas@hotmail.com>
2023-08-31 13:35:54 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Muuda <code>src/App.tsx</code> ja salvesta.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Test
         </a>
       </header>
     </div>
